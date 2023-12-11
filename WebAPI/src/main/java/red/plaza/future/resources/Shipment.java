2a1af02578f7a59963b4609a0a353e25edf05cf0
nimysan <234aini@gmail.com>
2023-12-11 16:35:20 +0800

    test

--- WebAPI/src/main/java/red/plaza/future/resources/Shipment.java
+++ WebAPI/src/main/java/red/plaza/future/resources/Shipment.java
@@ -1,2 +1,21 @@
-package red.plaza.future.resources.representational;public class Shipment {
+package red.plaza.future.resources;
+
+import jakarta.inject.Singleton;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import lombok.extern.slf4j.Slf4j;
+import red.plaza.future.resources.representational.OrderState;
+
+@Slf4j
+@Singleton
+public class Shipment {
+    @PUT
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response load(@PathParam("id") String orderId) {
+        OrderState orderState = OrderState.builder().orderId(orderId).orderDesc("hello").build();
+        log.info("--- {}", orderState);
+        return Response.status(200).entity(orderState).build();
+    }
 }
